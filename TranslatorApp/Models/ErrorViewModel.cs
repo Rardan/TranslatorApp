043548cc04dd04b5b07f9cf1f8a37d52f494256b
Rardan <przemolex01@gmail.com>
2020-07-12 11:37:43 +0200

    Add project files.

---
+++ TranslatorApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TranslatorApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
